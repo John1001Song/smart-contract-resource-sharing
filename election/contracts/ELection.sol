61ae5b236b99fb1a2ed1af68d79cab96e60b2910
yj.ge <yj.ge@tianrang-inc.com>
2019-12-31 13:29:50 +0800

    store candidates into map

--- election/contracts/ELection.sol
+++ election/contracts/ELection.sol
@@ -4,8 +4,24 @@ contract Election {
     // Read/write candidate
     string public candidate;
 
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
