c7a5052e900fc23b2d0152387461433f32d19e2a
CharlesGe129 <char1209ge@gmail.com>
2020-01-04 11:35:36 +0800

    add provider: tests

--- test/resource_sharing.js
+++ test/resource_sharing.js
@@ -38,4 +38,76 @@ contract("ResourceSharing", function(accounts) {
       assert('0x0', provider.next);
     });
   });
+
+  it("add provider in middle", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      rsInstance = instance;
+      return rsInstance.addProvider("hello", 1, 2, 3);
+    }).then(function(addEvent) {
+      // console.log("json=", JSON.stringify(addEvent))
+      return rsInstance.addProvider("test", 7, 8, 9);
+    }).then(function(addEvent) {
+      // console.log("json=", JSON.stringify(addEvent))
+      return rsInstance.addProvider("world", 4, 5, 6);
+    }).then(function(addEvent) {
+      // console.log("json=", JSON.stringify(addEvent))
+      return rsInstance.head();
+    }).then(function(head) {
+      return rsInstance.providerList(head);
+    }).then(function(provider) {
+      assert("hello", provider.name);
+      assert(1, provider.target);
+      assert(2, provider.start);
+      assert(3, provider.end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert("world", provider.name);
+      assert(4, provider.target);
+      assert(5, provider.start);
+      assert(6, provider.end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert("test", provider.name);
+      assert(7, provider.target);
+      assert(8, provider.start);
+      assert(9, provider.end);
+      assert('0x0', provider.next);
+    });
+  });
+
+  it("add provider in reverse order", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      rsInstance = instance;
+      return rsInstance.addProvider("test", 7, 8, 9);
+    }).then(function(addEvent) {
+      // console.log("json=", JSON.stringify(addEvent))
+      return rsInstance.addProvider("world", 4, 5, 6);
+    }).then(function(addEvent) {
+      // console.log("json=", JSON.stringify(addEvent))
+      return rsInstance.addProvider("hello", 1, 2, 3);
+    }).then(function(addEvent) {
+      // console.log("json=", JSON.stringify(addEvent))
+      return rsInstance.head();
+    }).then(function(head) {
+      return rsInstance.providerList(head);
+    }).then(function(provider) {
+      assert("hello", provider.name);
+      assert(1, provider.target);
+      assert(2, provider.start);
+      assert(3, provider.end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert("world", provider.name);
+      assert(4, provider.target);
+      assert(5, provider.start);
+      assert(6, provider.end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert("test", provider.name);
+      assert(7, provider.target);
+      assert(8, provider.start);
+      assert(9, provider.end);
+      assert('0x0', provider.next);
+    });
+  });
 });
