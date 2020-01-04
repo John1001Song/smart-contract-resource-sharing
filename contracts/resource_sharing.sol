7dddfa9d239477403dad2d82062063e8e8946a24
CharlesGe129 <char1209ge@gmail.com>
2020-01-04 18:52:18 +0800

    (1) addConsumer(); (2) fix addProvider()

--- contracts/resource_sharing.sol
+++ contracts/resource_sharing.sol
@@ -20,40 +20,52 @@ contract ResourceSharing is Logger {
         bytes32 next;
 
         string name;
-
+        address addr;
+        uint budget;
+        uint duration;
+        uint deadline;
     }
 
+    struct Matching {
+        string providerName;
+        address providerAddr;
+        string consumerName;
+        address consumerAddr;
+        uint256 price;
+        uint matchedTime;
+        uint start;
+        uint duration;
+    }
+
+    uint256 public maxMatchInterval;
     bytes32 public head;
     mapping (bytes32 => Provider) public providerList;
-
-    constructor() public {}
+    mapping (address => Matching) public matchings;
 
     event AddProvider(bytes32 id, bytes32 next, string _name, address _addr, uint _target, uint _start, uint _end);
+    event Matched(string providerName, address providerAddr, string consumerName, address consumerAddr, uint256 price, uint time, uint start, uint duration);
+
+    constructor() public {
+        // 100 seconds
+        maxMatchInterval = 100 * 1000;
+    }
 
     function addProvider(string memory _name, uint _target, uint _start, uint _end) public returns (bool) {
-        bytes32 curBytes = head;
-        bytes32 nextBytes;
-        Provider memory current;
-        Provider memory next;
-
-        // remove expired providers
-        while (true) {
-            current = providerList[curBytes];
-            if (curBytes == 0x0 || current.end > now) {
-                break;
-            }
-            curBytes = current.next;
-        }
-        head = curBytes;
-
-        // validate parameters
         require(_end > now, "bad end time");
         require(_start < _end, "bad start time");
 
-        // empty provider list
-        if (head == 0x0) {
-            bytes32 id = keccak256(abi.encodePacked(_name, _target, _start, _end));
-            Provider memory provider = Provider(id, 0x0, _name, msg.sender, _target, _start, _end);
+        // remove expired providers
+        removeExpiredProviders();
+
+        bytes32 curBytes = head;
+        bytes32 nextBytes;
+        Provider memory current = providerList[head];
+        Provider memory next;
+        bytes32 id = keccak256(abi.encodePacked(_name, _target, _start, _end, now));
+
+        // Do insertion at head when (1) empty provider list; (2) Or, _end <= first end
+        if (head == 0x0 || _end <= current.end) {
+            Provider memory provider = Provider(id, current.id, _name, msg.sender, _target, _start, _end);
             head = id;
             providerList[id] = provider;
             // logProvider("add provider", provider);
@@ -68,8 +80,8 @@ contract ResourceSharing is Logger {
             next = providerList[nextBytes];
             if (nextBytes == 0x0 || _end <= next.end) {
                 // Do insertion when (1) reached the end of the linked list; (2) Or, insert between current and next
-                bytes32 id = keccak256(abi.encodePacked(_name, _target, _start, _end));
                 current.next = id;
+                providerList[curBytes] = current;
                 Provider memory provider = Provider(id, nextBytes, _name, msg.sender, _target, _start, _end);
                 providerList[id] = provider;
                 // logProvider("add provider", provider);
@@ -79,6 +91,56 @@ contract ResourceSharing is Logger {
             curBytes = nextBytes;
         }
         return false;
+    }
+
+    function addConsumer(string memory _name, uint _budget, uint _duration, uint _deadline) public returns (bool) {
+        require(now + _duration + maxMatchInterval < _deadline, "not enough time to consume resource");
+        
+        // remove expired providers
+        removeExpiredProviders();
+
+        bytes32 curBytes = head;
+        Provider memory provider;
+        while (true) {
+            if (curBytes == 0x0) {
+                break;
+            }
+            provider = providerList[curBytes];
+            if (provider.target < _budget) {
+                curBytes = provider.next;
+                continue;
+            }
+            if (provider.start + maxMatchInterval + _duration < provider.end ) {
+                // matched
+                Matching memory m = Matching(provider.name, provider.addr, _name, msg.sender, _budget, now, provider.start, _duration);
+                matchings[provider.addr] = m;
+                matchings[msg.sender] = m;
+                emit Matched(provider.name, provider.addr, _name, msg.sender, _budget, now, provider.start, _duration);
+
+                // increase provider's start
+                // TODO: if new start > end, remove provider
+                provider.start += maxMatchInterval + _duration;
+                providerList[provider.id] = provider;
+                return true;
+            } else {
+                curBytes = provider.next;
+            }
+        }
+        return false;
+    }
+
+    function removeExpiredProviders() public {
+        bytes32 curBytes = head;
+        Provider memory current = providerList[head];
+
+        while (true) {
+            current = providerList[curBytes];
+            if (curBytes == 0x0 || current.end > now) {
+                break;
+            }
+            curBytes = current.next;
+        }
+        head = curBytes;
     }
 
     function logProvider(string memory msg, Provider memory provider) private {
--- test/resource_sharing.js
+++ test/resource_sharing.js
@@ -11,66 +11,9 @@ contract("ResourceSharing", function(accounts) {
   var rsInstance;
   var start = 7999999999;
   var end = 9999999999;
+  var maxMatchInterval = 100 * 1000;
 
-  it("add providers", function() {
-    return resourseSharing.deployed().then(function(instance) {
-      rsInstance = instance;
-      return rsInstance.addProvider("hello", 1, start, end);
-    }).then(function(addEvent) {
-      // console.log("json=", JSON.stringify(addEvent))
-      return rsInstance.addProvider("world", 2, start, end);
-    }).then(function(addEvent) {
-      // console.log("json=", JSON.stringify(addEvent))
-      return rsInstance.addProvider("test", 3, start, end);
-    }).then(function(addEvent) {
-      // console.log("json=", JSON.stringify(addEvent))
-      return rsInstance.head();
-    }).then(function(head) {
-      return rsInstance.providerList(head);
-    }).then(function(provider) {
-      assert("hello", provider.name);
-      assert(1, provider.target);
-      assert(start, provider.start);
-      assert(end, provider.end);
-      return rsInstance.providerList(provider.next);
-    }).then(function(provider) {
-      assert("world", provider.name);
-      assert(4, provider.target);
-      assert(start, provider.start);
-      assert(end, provider.end);
-      return rsInstance.providerList(provider.next);
-    }).then(function(provider) {
-      assert("test", provider.name);
-      assert(7, provider.target);
-      assert(start, provider.start);
-      assert(end, provider.end);
-      assert('0x0', provider.next);
-    });
-  });
-
-  it("remove expired providers", function() {
-    return resourseSharing.deployed().then(function(instance) {
-      rsInstance = instance;
-      var start = Math.round(new Date().getTime() / 1000) + 1;
-      rsInstance.addProvider("hello", 1, start, start + 1);
-      return rsInstance.head();
-    }).then(function(head) {
-      return rsInstance.providerList(head);
-    }).then(function(provider) {
-      assert("hello", provider.name);
-      sleep(2000);
-      rsInstance.addProvider("test", 3, start, end);
-      return rsInstance.head();
-    }).then(function(addEvent) {
-      return rsInstance.head();
-    }).then(function(head) {
-      return rsInstance.providerList(head);
-    }).then(function(provider) {
-      assert("test", provider.name);
-    });
-  });
-
-  it("bad parameter end", function() {
+  it("bad provider parameter end", function() {
     return resourseSharing.deployed().then(function(instance) {
       rsInstance = instance;
       return rsInstance.addProvider("hello", 1, 1, 1);
@@ -79,12 +22,136 @@ contract("ResourceSharing", function(accounts) {
     });
   });
 
-  it("bad parameter start", function() {
+  it("bad provider parameter start", function() {
     return resourseSharing.deployed().then(function(instance) {
       rsInstance = instance;
       return rsInstance.addProvider("hello", 1, 9999999999, 7999999999);
     }).then(assert.fail).catch(function(error) {
       assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+    });
+  });
+
+  it("add providers", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      rsInstance = instance;
+      return rsInstance.addProvider("hello", 3, start, end, { from: accounts[0] });
+    }).then(function(addEvent) {
+      return rsInstance.addProvider("world", 2, start, end, { from: accounts[0] });
+    }).then(function(addEvent) {
+      return rsInstance.addProvider("test", 1, start, end, { from: accounts[0] });
+    }).then(function(addEvent) {
+      return rsInstance.addProvider("provider4", 4, start, end+1, { from: accounts[0] });
+    }).then(function(addEvent) {
+      return rsInstance.head();
+    }).then(function(head) {
+      return rsInstance.providerList(head);
+    }).then(function(provider) {
+      assert.equal(provider.name, "test");
+      assert.equal(provider.target, 1);
+      assert.equal(provider.start, start);
+      assert.equal(provider.end, end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert.equal(provider.name, "world");
+      assert.equal(provider.target, 2);
+      assert.equal(provider.start, start);
+      assert.equal(provider.end, end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert.equal(provider.name, "hello");
+      assert.equal(provider.target, 3);
+      assert.equal(provider.start, start);
+      assert.equal(provider.end, end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert.equal(provider.name, "provider4");
+      assert.equal(provider.target, 4);
+      assert.equal(provider.start, start);
+      assert.equal(provider.end, end+1);
+      assert.equal(provider.next, 0x0);
+    });
+  });
+
+  it("remove expired providers", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      rsInstance = instance;
+      var start = Math.round(new Date().getTime() / 1000);
+      rsInstance.addProvider("remove1", 1, start, start + 1);
+      return rsInstance.head();
+    }).then(function(head) {
+      return rsInstance.providerList(head);
+    }).then(function(provider) {
+      assert.equal(provider.name, "remove1");
+      sleep(2000);
+
+      // test remove when adding
+      var start = Math.round(new Date().getTime() / 1000);
+      return rsInstance.addProvider("remove2", 1, start, start + 1);
+    }).then(function(addProviderEvent) {
+      sleep(2000);
+
+      // test remove()
+      rsInstance.removeExpiredProviders();
+      return rsInstance.head();
+    }).then(function(head) {
+      return rsInstance.providerList(head);
+    }).then(function(provider) {
+      assert.equal(provider.name, "test");
+    });
+  });
+
+  it("bad consumer parameter duration and deadline", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      rsInstance = instance;
+      return rsInstance.addConsumer("hello", 1, 1000, 1);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+    });
+  });
+
+  it("add consumer", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      rsInstance = instance;
+      rsInstance.addConsumer("consumer1", 2, 100, end, { from: accounts[1] })
+      return rsInstance.head();
+    }).then(function(head) {
+      return rsInstance.providerList(head);
+    }).then(function(provider) {
+      assert.equal(provider.name, "test");
+      assert.equal(provider.target, 1);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert.equal(provider.name, "world");
+      assert.equal(provider.target, 2);
+      assert.equal(provider.start, start + maxMatchInterval + 100);
+      assert.equal(provider.end, end);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert.equal(provider.name, "hello");
+      assert.equal(provider.target, 3);
+      return rsInstance.providerList(provider.next);
+    }).then(function(provider) {
+      assert.equal(provider.name, "provider4");
+      assert.equal(provider.target, 4);
+      assert.equal(provider.next, 0x0);
+      return rsInstance.matchings(accounts[0]);
+    }).then(function(match) {
+      assert.equal(match.providerName, "world");
+      assert.equal(match.providerAddr, accounts[0]);
+      assert.equal(match.consumerName, "consumer1");
+      assert.equal(match.consumerAddr, accounts[1]);
+      assert.equal(match.price, 2);
+      assert.equal(match.start, start);
+      assert.equal(match.duration, 100);
+      return rsInstance.matchings(accounts[1]);
+    }).then(function(match) {
+      assert.equal(match.providerName, "world");
+      assert.equal(match.providerAddr, accounts[0]);
+      assert.equal(match.consumerName, "consumer1");
+      assert.equal(match.consumerAddr, accounts[1]);
+      assert.equal(match.price, 2);
+      assert.equal(match.start, start);
+      assert.equal(match.duration, 100);
     });
   });
 });
