b2e9096fb532814a331b465713051db90d0f6240
CharlesGe129 <char1209ge@gmail.com>
2020-01-05 18:25:06 +0800

    add engagement test 100%

--- test/rs_performance.js
+++ test/rs_performance.js
@@ -2,8 +2,8 @@ var resourseSharing = artifacts.require("./ResourceSharing.sol");
 
 var rsInstance;
 var start = Math.round(new Date().getTime() / 1000);
-var end = 9999999999;
 var maxMatchInterval = 100 * 1000;
+var end = start + 300 * 1000;
 
 function sleep(delay) {
   var start = (new Date()).getTime();
@@ -16,7 +16,15 @@ function addProvider(accounts, num) {
   for (var i = 0; i < num; i ++) {
     var name = "Engagement #" + i;
     console.log("Add engagement: name=", name);
-    rsInstance.addProvider(name, 1, start, end, { from: accounts[0] });
+    rsInstance.addProvider(name, 1, start, start + 220 * 1000, { from: accounts[0] });
+  }
+}
+
+function addConsumer(accounts, num) {
+  for (var i = 0; i < num; i ++) {
+    var name = "consumer #" + i;
+    console.log("Add consumer, name:", name);
+    rsInstance.addConsumer(name, 1, 100 * 1000, start + 300 * 1000, { from: accounts[1] });
   }
 }
 
@@ -48,5 +56,33 @@ contract("ResourceSharing", function(accounts) {
 
   it("check length", function() {
     assert.equal(curBytes, 0x0);
+  })
+
+  console.log(num, "providers added")
+
+  it("add " + num + " consumer", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      rsInstance = instance;
+      addConsumer(accounts, num);
+    });
+  });
+
+  it("check match length", function() {
+    return resourseSharing.deployed().then(function(instance) {
+      return rsInstance.matchings(accounts[0], num-1);
+    }).then(function(match) {
+      assert.equal(match.providerAddr, accounts[0]);
+      assert.equal(match.consumerAddr, accounts[1]);
+      return rsInstance.matchings(accounts[1], num-1);
+    }).then(function(match) {
+      assert.equal(match.providerAddr, accounts[0]);
+      assert.equal(match.consumerAddr, accounts[1]);
+      return rsInstance.matchings(accounts[0], num);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return rsInstance.matchings(accounts[1], num);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+    });
   })
 });
