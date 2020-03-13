178c61ae8a38be240451354be175136314abf420
CharlesGe129 <char1209ge@gmail.com>
2020-03-13 17:29:32 +0800

    WIP: separate mode

--- contracts/resource_sharing.sol
+++ contracts/resource_sharing.sol
@@ -63,9 +63,11 @@ contract ResourceSharing is ProviderLib, StorageLib {
 
         bytes32 id = keccak256(abi.encodePacked(_name, _target, _start, _end, now));
 
-        // add indices
-        addProviderIndexModeMinLatency(headMap, providerMap, providerIndexMap, id, _region, _start, _end);
-        addProviderIndexModeMinCost(headMap, providerMap, providerIndexMap, id, _region, _target, _start, _end);
+        if (keccak256(bytes(_mode)) != keccak256(bytes("min_latency"))) {
+            addProviderIndexModeMinLatency(headMap, providerMap, providerIndexMap, id, _region, _start, _end);
+        } else if (keccak256(bytes(_mode)) == keccak256(bytes("min_cost"))) {
+            addProviderIndexModeMinCost(headMap, providerMap, providerIndexMap, id, _region, _target, _start, _end);
+        }
 
         // add provider
         Provider memory provider = Provider(id, _name, _region, msg.sender, _target, _start, _end);
@@ -78,15 +80,15 @@ contract ResourceSharing is ProviderLib, StorageLib {
     function addConsumer(string memory _mode, string memory _name, string memory _region, uint _budget, uint _duration, uint _deadline) public returns (bool) {
         require(now + _duration + maxMatchInterval < _deadline, "not enough time to consume resource");
 
-        if (keccak256(bytes(_mode)) != keccak256(bytes("min_latency")) &&
-        keccak256(bytes(_mode)) == keccak256(bytes("min_cost"))) {
-            return false;
+        if (keccak256(bytes(_mode)) != keccak256(bytes("min_latency")) {
+            return addConsumerByModeMinLatency(_name, _region, _budget, _duration);
+        } else if (keccak256(bytes(_mode)) == keccak256(bytes("min_cost"))) {
+            return addConsumerByModeMinCost(_mode, _name, _region, _budget, _duration);
         }
-        return addConsumerByMode(_mode, _name, _region, _budget, _duration);
     }
 
 
-    function addConsumerByMode(string memory _mode, string memory _name, string memory _region, uint _budget, uint _duration) public returns (bool) {
+    function addConsumerByModeMinLatency(string memory _name, string memory _region, uint _budget, uint _duration) public returns (bool) {
         string[] memory strArr = new string[](3);
         strArr[0] = getProviderKey(_region, _mode);
         strArr[1] = _name;
@@ -105,11 +107,7 @@ contract ResourceSharing is ProviderLib, StorageLib {
         */
 
         // remove expired providers
-        if (keccak256(bytes(_mode)) != keccak256(bytes("min_latency"))) {
-            removeExpiredProvidersModeMinLatency(headMap, providerMap, providerIndexMap, strArr[0]);
-        } else if (keccak256(bytes(_mode)) == keccak256(bytes("min_cost"))) {
-            removeExpiredProviders(headMap, providerMap, providerIndexMap, strArr[0]);
-        }
+        removeExpiredProvidersModeMinLatency(headMap, providerMap, providerIndexMap, strArr[0]);
 
         if (headMap[strArr[0]] == 0x0) {
             // TODO: if no matching under current region, search adjacent regions
@@ -160,199 +158,6 @@ contract ResourceSharing is ProviderLib, StorageLib {
 
     function removeExpiredProvidersModeMinLatency(string memory _key) public {
         removeExpiredProvidersModeMinLatency(headMap, providerMap, providerIndexMap, _key);
-    }
-    
-    struct Provider {
-        bytes32 id;
-        string name;
-        string region;
-        address addr;
-        uint target;
-        uint start;
-        uint end;
-    }
-
-    struct ProviderIndex {
-        bytes32 id;
-        bytes32 next;
-    }
-
-    /////////////////////////////////////////////////////////// MODE MIN_LATENCY ///////////////////////////////////////////////////////////
-
-    function addProviderIndexModeMinLatency(mapping(string => bytes32) storage headMap,
-        mapping(bytes32 => Provider) storage providerMap,
-        mapping(string => mapping(bytes32 => ProviderIndex)) storage providerIndexMap,
-        bytes32 id, string memory _region, uint _start, uint _end) internal returns (bool) {
-        string memory key = getProviderKey(_region, "min_latency");
-
-        // remove expired providers
-        removeExpiredProvidersModeMinLatency(headMap, providerMap, providerIndexMap, key);
-
-        Provider memory current = providerMap[headMap[key]];
-        Provider memory next;
-        ProviderIndex memory curIndex;
-        ProviderIndex memory newIndex;
-
-        // check head
-        if (isBetweenCurrentAndNext(headMap[key], _start, _end, current.start, current.end)) {
-            newIndex = ProviderIndex(id, current.id);
-            headMap[key] = id;
-            providerIndexMap[key][id] = newIndex;
-            return true;
-        }
-
-        // iterate
-        while (true) {
-            curIndex = providerIndexMap[key][current.id];
-            next = providerMap[curIndex.next];
-            if (isBetweenCurrentAndNext(next.id, _start, _end, next.start, next.end)) {
-                newIndex = ProviderIndex(id, curIndex.next);
-                providerIndexMap[key][id] = newIndex;
-                providerIndexMap[key][current.id].next = newIndex.id;
-                return true;
-            }
-            current = providerMap[curIndex.next];
-        }
-        return false;
-    }
-
-    // Only used for mode MIN_LATENCY
-    function removeExpiredProvidersModeMinLatency(mapping(string => bytes32) storage headMap,
-        mapping(bytes32 => Provider) storage providerMap,
-        mapping(string => mapping(bytes32 => ProviderIndex)) storage providerIndexMap,
-        string memory _key) internal {
-        bytes32 curBytes = headMap[_key];
-        Provider memory current;
-        ProviderIndex memory index;
-
-        while (true) {
-            current = providerMap[curBytes];
-            if (curBytes == 0x0 || current.end > now) {
-                break;
-            }
-            index = providerIndexMap[_key][curBytes];
-            curBytes = index.next;
-            delete (providerMap[index.id]);
-            delete (providerIndexMap[_key][index.id]);
-        }
-        headMap[_key] = curBytes;
-    }
-
-    // Only used for mode MIN_LATENCY
-    function isBetweenCurrentAndNext(bytes32 _nextBytes, uint _start, uint _end, uint _nextStart, uint _nextEnd) private returns (bool) {
-        return _nextBytes == 0x0 || _end < _nextEnd || (_end == _nextEnd && _start <= _nextStart);
-    }
-
-    /////////////////////////////////////////////////////////// MODE MIN_COST ///////////////////////////////////////////////////////////
-
-    function addProviderIndexModeMinCost(mapping(string => bytes32) storage headMap,
-        mapping(bytes32 => Provider) storage providerMap,
-        mapping(string => mapping(bytes32 => ProviderIndex)) storage providerIndexMap,
-        bytes32 id, string memory _region, uint _target, uint _start, uint _end) internal returns (bool) {
-
-        string memory key = getProviderKey(_region, "min_cost");
-
-        Provider memory current = providerMap[headMap[key]];
-        Provider memory next;
-        ProviderIndex memory curIndex = providerIndexMap[key][headMap[key]];
-        ProviderIndex memory newIndex;
-
-        // get first valid provider index, remove expired
-        while (true) {
-            if (current.id != 0x0) {
-                break;
-            }
-            if (curIndex.id != 0x0) {
-                delete (providerIndexMap[key][curIndex.id]);
-                headMap[key] = curIndex.next;
-                current = providerMap[curIndex.next];
-                curIndex = providerIndexMap[key][curIndex.next];
-            } else {
-                break;
-            }
-        }
-        // check first
-        if (isBetweenCurrentAndNextModeMinCost(headMap[key], _target, current.target, _start, _end, current.start, current.end)) {
-            newIndex = ProviderIndex(id, current.id);
-            headMap[key] = id;
-            providerIndexMap[key][id] = newIndex;
-            return true;
-        }
-
-        // iterate
-        while (true) {
-            curIndex = providerIndexMap[key][current.id];
-            next = providerMap[curIndex.next];
-            // check next provider is valid
-            while (next.id == 0x0 && curIndex.next != 0x0) {
-                // link current and next.next
-                newIndex = providerIndexMap[key][curIndex.next];
-                delete (providerIndexMap[key][newIndex.id]);
-                curIndex.next = newIndex.next;
-                providerIndexMap[key][curIndex.id] = curIndex;
-                next = providerMap[curIndex.next];
-            }
-            if (curIndex.next == 0x0 || isBetweenCurrentAndNextModeMinCost(next.id, _target, next.target, _start, _end, next.start, next.end)) {
-                newIndex = ProviderIndex(id, curIndex.next);
-                providerIndexMap[key][id] = newIndex;
-                providerIndexMap[key][current.id].next = newIndex.id;
-                return true;
-            }
-            current = providerMap[curIndex.next];
-        }
-        return false;
-    }
-
-    function isBetweenCurrentAndNextModeMinCost(bytes32 _nextBytes, uint _target, uint _nextTarget, uint _start, uint _end, uint _nextStart, uint _nextEnd) private returns (bool) {
-        if (_nextBytes == 0x0) {
-            return true;
-        }
-        if (_target > _nextTarget) {
-            return false;
-        }
-        return _end < _nextEnd || (_end == _nextEnd && _start <= _nextStart);
-    }
-
-    // Only used for mode MIN_LATENCY
-    function removeExpiredProviders(mapping(string => bytes32) storage headMap,
-        mapping(bytes32 => Provider) storage providerMap,
-        mapping(string => mapping(bytes32 => ProviderIndex)) storage providerIndexMap,
-        string memory _key) internal {
-        bytes32 curBytes = headMap[_key];
-        Provider memory current;
-        ProviderIndex memory index;
-
-        while (true) {
-            current = providerMap[curBytes];
-            if (curBytes == 0x0 || current.end > now) {
-                break;
-            }
-            index = providerIndexMap[_key][curBytes];
-            curBytes = index.next;
-            delete (providerMap[index.id]);
-            delete (providerIndexMap[_key][index.id]);
-        }
-        headMap[_key] = curBytes;
-    }
-
-    /////////////////////////////////////////////////////////// COMMON ///////////////////////////////////////////////////////////
-
-    function getProviderKey(string memory _region, string memory _mode) internal returns (string memory) {
-        return stringConcat(_region, "||", _mode);
-    }
-
-    function stringConcat(string memory a, string memory b, string memory c) internal pure returns (string memory) {
-        return string(abi.encodePacked(a, b, c));
-    }
-
-    function removeProviderIndices(mapping(bytes32 => Provider) storage providerMap,
-        mapping(string => mapping(bytes32 => ProviderIndex)) storage providerIndexMap,
-        string memory _region, bytes32 _id) internal {
-        delete (providerMap[_id]);
-
-        // delete indices of all modes
-        delete (providerIndexMap[getProviderKey(_region, "min_latency")][_id]);
-        delete (providerIndexMap[getProviderKey(_region, "min_cost")][_id]);
     }
 
 
